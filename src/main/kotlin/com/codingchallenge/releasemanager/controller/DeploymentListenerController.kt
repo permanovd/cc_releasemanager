93c76bc63b9e91983d4d0f20cb1930a0cb1026a5
David Permanov <david.permanov@infobip.com>
2023-09-11 00:49:55 +0200

    Add logging and monitoring.

--- src/main/kotlin/com/codingchallenge/releasemanager/controller/DeploymentListenerController.kt
+++ src/main/kotlin/com/codingchallenge/releasemanager/controller/DeploymentListenerController.kt
@@ -1,15 +1,19 @@
 package com.codingchallenge.releasemanager.controller
 
 import com.codingchallenge.releasemanager.domain.VersionTrackingService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
+import kotlin.Exception
 
 
 @RestController
 class DeploymentListenerController(private val versionTrackingService: VersionTrackingService) {
+    private val log = KotlinLogging.logger {}
     @PostMapping("/deploy")
     fun onDeploy(@RequestBody payload: DeploymentInfo): Mono<OverallVersionDto> {
+        log.info { "deploy is notified with payload: $payload" }
         return versionTrackingService
             .newVersionDeployed(payload.serviceName, payload.serviceVersionNumber)
             .map { OverallVersionDto(it.versionId, it.serviceVersions.map { v -> DeployedVersion(v.key, v.value) }) }
@@ -17,6 +21,7 @@ class DeploymentListenerController(private val versionTrackingService: VersionTr
 
     @GetMapping("services")
     fun getVersion(@RequestParam("systemVersion") version: Long): Mono<List<DeployedVersion>> {
+        log.info { "Version fetch is called with $version" }
         return versionTrackingService
             .overallVersionByNumber(version)
             .switchIfEmpty(Mono.error(NotFoundException()))
@@ -28,6 +33,12 @@ class DeploymentListenerController(private val versionTrackingService: VersionTr
     fun notFoundExceptionHandler() {
     }
 
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Failed to process request")
+    @ExceptionHandler(Exception::class)
+    fun unexpectedExceptionHandler(e: Exception): String {
+        log.warn(e) { "Failed to process request" }
+        return "Failed to process request"
+    }
 }
 
 data class DeployedVersion(val name: String, val version: Long)
